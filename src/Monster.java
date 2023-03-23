c412648e3dd9f4b1837f270b808dc8364ec50c95
sancruzz <125682544+sancruzz@users.noreply.github.com>
2023-03-23 01:51:41 +0100

    variablerna //Testing

--- src/Monster.java
+++ src/Monster.java
@@ -2,4 +2,30 @@ public class Monster {
 
     // hej
     // Monster-class
+import java.lang.reflect.Method;
+
+public class Monster {
+
+
+    // Detta är Monster klassen
+
+    // variabler (monstret förlorar Gold vid döden,monstret gör att användaren förlorar hp men vinner xp)
+
+
+    public String name;
+    public int HP;
+    public int XP;
+
+    //  public Gold Gold;
+
+
+
+
+
+
+
+
+
+
+
 }
