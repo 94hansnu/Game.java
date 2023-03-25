6ba7bc050fe6c0cbab34ebcaa4da956ce490a851
sancruzz <125682544+sancruzz@users.noreply.github.com>
2023-03-25 12:12:07 +0100

    .

--- src/Monster.java
+++ src/Monster.java
@@ -1,4 +1,39 @@
+import java.lang.reflect.Method;
+
 public class Monster {
+
+
+    // Detta är Monster klassen
+
+    // variabler (monstret förlorar Gold vid döden,monstret gör att användaren förlorar hp men vinner xp)
+
+
+    public String name;
+    public int HP;
+    public int XP;
+
+
+    public Monster (String name, int HP, int XP)
+
+    public void getXP()
+
+    // public void lessXP
+
+    public int getXP ()
+       // return 0?
+    public void defense(int attack)  {hp-=attack; }
+
+
+
+
+
+
+
+
+
+
+
+
 
     // hej
     // Monster-class
