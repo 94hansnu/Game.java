045e57a053d0dbf6eab45939c72667de4a48f80f
liz9922 <124145079+liz9922@users.noreply.github.com>
2023-03-25 12:55:01 +0100

    städar

--- src/Monster.java
+++ src/Monster.java
@@ -1,38 +1,52 @@
 import java.lang.reflect.Method;
-
-public class Monster {
+// extenda
+public class Monster extends Traits{
 
 
     // Detta är Monster klassen
 
     // variabler (monstret förlorar Gold vid döden,monstret gör att användaren förlorar hp men vinner xp)
 
-
+    // dessa tre borde raderas om hanandi har gjort som vi skrev
     public String name;
     public int HP;
     public int XP;
 
+    /* metoderna som ska finnas:
+    void attack();
 
-    public Monster (String name, int HP, int XP)
+ void defense(Integer attack);
 
-    public void getXP()
+ String getName();
+
+ Integer getXp();
+
+ Integer getHp();
+     */
+
+
+    // borde här ej vara:
+    public Monster (String name, int HP, int XP) {
+        super(name,hp,xp);
+    }
+    // int ? public int
+    @Override
+    public int getXP() { return xp; }
 
     // public void lessXP
 
     public int getXP ()
        // return 0?
     //utveckl
+    @Override
     public void defense(int attack)  {hp-=attack; }
-
-
-
-
-
-
-
-
-
-
+    @Override
+    public int getHP() { return HP; }
+    @Override
+    public String getName() { return name; }
+    @Override
+    public Integer attack() { return 1+(int)(Math.random()*10); }
+}
 
 
 
