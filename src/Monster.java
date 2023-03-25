ddf4261d184ce9c0a55d576bc0c508076299c51f
liz9922 <124145079+liz9922@users.noreply.github.com>
2023-03-25 13:14:55 +0100

    .

--- src/Monster.java
+++ src/Monster.java
@@ -1,5 +1,4 @@
-import java.lang.reflect.Method;
-// extenda
+
 public class Monster extends Traits{
 
 
@@ -24,78 +23,11 @@ public class Monster extends Traits{
  Integer getHp();
      */
 
-
-    // borde här ej vara:
     public Monster (String name, int HP, int XP) {
         super(name,hp,xp);
     }
-    // int ? public int
     @Override
     public int getXP() { return xp; }
-
-    // public void lessXP
-
-    public int getXP ()
-       // return 0?
-    //utveckl
-    @Override
-    public void defense(int attack)  {hp-=attack; }
-    @Override
-    public int getHP() { return HP; }
-    @Override
-    public String getName() { return name; }
-    @Override
-    public Integer attack() { return 1+(int)(Math.random()*10); }
-}
-
-
-
-    // hej
-    // Monster-class
-public class Monster {
-
-    // hej
-    // Monster-class
-}
-import java.lang.reflect.Method;
-// extenda
-public class Monster extends Traits{
-
-
-    // Detta är Monster klassen
-
-    // variabler (monstret förlorar Gold vid döden,monstret gör att användaren förlorar hp men vinner xp)
-
-    // dessa tre borde raderas om hanandi har gjort som vi skrev
-    public String name;
-    public int HP;
-    public int XP;
-
-    /* metoderna som ska finnas:
-    void attack();
-
- void defense(Integer attack);
-
- String getName();
-
- Integer getXp();
-
- Integer getHp();
-     */
-
-
-    // borde här ej vara:
-    public Monster (String name, int HP, int XP) {
-        super(name,hp,xp);
-    }
-    // int ? public int
-    @Override
-    public int getXP() { return xp; }
-
-    // public int lessHP
-    public int getXP ()
-       // return 0?
-    //utveckl
     @Override
     public void defense(int attack)  {hp-=attack; }
     @Override
