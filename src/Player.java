b2c7fcde6e2fc5970025aa876cfff82f4fe482d8
94hansnu <124634856+94hansnu@users.noreply.github.com>
2023-03-27 10:37:09 +0200

    Player

--- src/Player.java
+++ src/Player.java
@@ -1,18 +1,15 @@
 public class Player extends Traits{
-    private Integer gold;
+    private Gold gold;
     private Integer strength;
     private Integer toughness;
-    public Player(String name, Integer xp, Integer hp, Integer gold, Integer strength, Integer toughness){
+    public Player(String name, Integer xp, Integer hp ){
         super(name, xp, hp);
-        this.gold=gold;
-        this.strength=strength;
-        this.toughness=toughness;
+        Gold= new Gold;
+        this.strength=0;
+        this.toughness=0;
     }
     @Override
-    public void defense(){
-        int damage= 2* (getHp()/toughness);
-        setHp(getHp() - damage);
-        System.out.println(getName() + " lost " + damage + " HP due to defense.");
+    public void defense(int damage){
     }
     public Integer getGold(){
         return gold;
@@ -25,10 +22,8 @@ public class Player extends Traits{
     }
 }
 //Pseudokod
-//Instansvariabler för player-klassen, Integer gold, Integer strength, Integer toughness
-//Konstrukor för player-klassen, name, xp, hp, gold, stength och toughness
+//Instansvariabler för player-klassen
+//Konstrukor för player-klassen
 //Superklass för att kalla på Traits klassens konstruktor och sedan initialisera player klassens egna instansvariabler
-//Override-metoden defense som finns i Traits klassen och beräkna mängd skada som spelaren kommer
-// att ta med beaktning av toughness instansvariabel. Uppdatera spelarens hp instansvariabel och skriv ut
-// hur mycket skada som har uppstått.
+//Override-metoden defense som finns i Traits klassen
 //Getter-metoder för player-instansvariabler
