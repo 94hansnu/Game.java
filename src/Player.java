1fe31513dd0a4ec2ed317ce93451287fe8688113
94hansnu <124634856+94hansnu@users.noreply.github.com>
2023-03-27 10:54:59 +0200

    Player

--- src/Player.java
+++ src/Player.java
@@ -4,26 +4,26 @@ public class Player extends Traits{
     private Integer toughness;
     public Player(String name, Integer xp, Integer hp ){
         super(name, xp, hp);
-        Gold= new Gold;
+        Gold= new Gold();
         this.strength=0;
         this.toughness=0;
     }
     @Override
     public void defense(int damage){
     }
-    public Integer getGold(){
-        return gold;
-    }
+
     public Integer getStrength(){
         return strength;
     }
     public Integer getToughness(){
         return toughness;
     }
+    public void  setStrength(Integer strength){ this.strength;}
+    public void setToughness(Integer toughness){ this.toughness;}
 }
 //Pseudokod
 //Instansvariabler för player-klassen
 //Konstrukor för player-klassen
 //Superklass för att kalla på Traits klassens konstruktor och sedan initialisera player klassens egna instansvariabler
 //Override-metoden defense som finns i Traits klassen
-//Getter-metoder för player-instansvariabler
+//Getter och setters -metoder för player-instansvariabler
