4d25ba3795be1f1c41b76a87255e08c382b4cb29
94hansnu <124634856+94hansnu@users.noreply.github.com>
2023-03-28 23:01:48 +0200

    ny

--- src/Player.java
+++ src/Player.java
@@ -2,29 +2,36 @@ public class Player extends Traits{
     private Gold gold;
     private Integer strength;
     private Integer toughness;
+    private int defense;
     public Player(String name, Integer xp, Integer hp ){
         super(name, xp, hp);
-        Gold= new Gold();
+        Gold gold= new Gold();
         this.strength=0;
         this.toughness=0;
     }
-
     @Override
-    public void defense(int damage){
-        if(damage + toughness < 0) {
-            setHp(getXp()-=(damage+toughness));
-        }
-
+    public void defense(){//osäker om denna ska vara kvar
     }
-
+    public void defense(int damage){
+        int totalDefense= defense+toughness;//beräknar totalt försvar inklusive toughness
+        int effectiveDamage= damage-totalDefense;//beräknar effektiv skada efter försvar
+        if (effectiveDamage>0){
+            int newHp=getHp()-effectiveDamage;//beräknar ny hp efter skada
+            setHp(newHp);//sätter nya hp värdet
+        }
+    }
     public Integer getStrength(){
         return strength;
     }
     public Integer getToughness(){
         return toughness;
     }
-    public void  setStrength(Integer strength){ this.strength;}
-    public void setToughness(Integer toughness){ this.toughness;}
+    public void  setStrength(Integer strength){
+        this.strength+=strength;
+    }
+    public void setToughness(Integer toughness){
+        this.toughness+=toughness;
+    }
 }
 //Pseudokod
 //Instansvariabler för player-klassen
